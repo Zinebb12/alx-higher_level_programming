55aa5fd3ac9c23a7cf1c77d9ed32acc3e62f1c6f
Zinebb12 <zinebzaid@gmail.com>
2023-11-28 08:56:28 -0800

    Inventing is a combination of brains

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 
 for first_numbers in range(0, 10):
-   for second_numbers in range(first_numbers + 1, 10):
-      if first_numbers == 8 and second_numbers == 9:
-          print("{}{}".format(first_numbers, second_numbers))
-      else:
-          print("{}{}".format((first_numbers, second_numbers), end=", ")
+     for second_numbers in range(first_numbers + 1, 10):
+       if first_numbers == 8 and second_numbers == 9:
+           print("{}{}".format(first_numbers, second_numbers))
+       else:
+           print("{}{}".format(first_numbers, second_numbers), end=", ")
