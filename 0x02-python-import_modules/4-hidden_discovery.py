143fc76c5a9b9c813590c0fab1e3cfecd8e2680c
Zinebb12 <zinebzaid@gmail.com>
2023-11-30 15:16:28 -0800

    wa test

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -3,6 +3,6 @@ if __name__ == "__main__":
     import hidden_4
 
     content = dir(hidden_4)
-    for i in content:
-         if i[:2] != '__'
+    for title in content:
+         if title[:2] != '__'
               print(i)
