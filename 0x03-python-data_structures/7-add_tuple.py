bd00623f6582876333460e06d19e0e4726f57003
Zinebb12 <zinebzaid@gmail.com>
2023-12-04 08:57:43 -0800

    akhir test

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -4,12 +4,12 @@ def add_tuple(tuple_a=(), tuple_b=()):
   
   if len(tuple_a) < 2:
       tuple_a = (tuple_a[0], 0)
-  #elif len(tuple_a) < 1:
-        #tuple_a = (0, 0)
+  elif len(tuple_a) < 1:
+        tuple_a = (0, 0)
 
 # check for tuple_b
-  #if len(tuple_b) < 1:
-      #tuple_b = (0, 0)
+  if len(tuple_b) < 1:
+      tuple_b = (0, 0)
   elif len(tuple_b) < 2:
       tuple_b = (tuple_b[0], 0)
 
