00ffd026de1a034e2fda5316891cc44acfb2f8d4
Zinebb12 <zinebzaid@gmail.com>
2023-12-19 16:27:06 -0800

    Square with size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""create a private attribute with the class square"""
+
+
+class Square:
+    """define a private attribute"""
+    def __init__(self, size):
+        self.__size = size
