8cc24249b3083f0a8d6fa8d65bd0acd8acf5d6ea
Zinebb12 <zinebzaid@gmail.com>
2023-12-19 16:34:07 -0800

    Size validation

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""make some change on the private attribute"""
+
+
+class Square:
+    """check if the size match with the requerment"""
+    def __init__(self, size=0):
+        self.__size = size
+        if type(self.__size) != int:
+            raise TypeError("size must be an integer")
+        elif self.__size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
