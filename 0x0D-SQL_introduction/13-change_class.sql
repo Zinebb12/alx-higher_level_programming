ce065886b0745290363afd0f3202919713866471
Zinebb12 <zinebzaid@gmail.com>
2024-02-13 18:49:44 -0800

    score too low

--- 0x0D-SQL_introduction/13-change_class.sql
+++ 0x0D-SQL_introduction/13-change_class.sql
@@ -1,4 +1,3 @@
-pt that removes all records with a 
+-- cript that removes all records with a 
 -- score <= 5 in the table second_table of the database hbtn_0c_0
 DELETE FROM second_table WHERE score <= 5;
-
