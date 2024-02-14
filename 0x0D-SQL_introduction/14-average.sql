91fe87cdfab56ec26f204a17e44c5aa4366def93
Zinebb12 <zinebzaid@gmail.com>
2024-02-13 18:50:36 -0800

    average

--- 0x0D-SQL_introduction/14-average.sql
+++ 0x0D-SQL_introduction/14-average.sql
@@ -1,3 +1,3 @@
-cript that computes the score average of 
+-- script that computes the score average of 
 -- all records in the table second_table of the database hbtn_0c_0
 SELECT AVG(score) as average FROM second_table;
