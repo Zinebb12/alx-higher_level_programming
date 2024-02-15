d9b0a6d824070aa4c03bbbf6f30d7bf2a01bb74f
Zinebb12 <zinebzaid@gmail.com>
2024-02-14 17:27:36 -0800

    Unique ID

--- 0x0E-SQL_more_queries/5-unique_id.sql
+++ 0x0E-SQL_more_queries/5-unique_id.sql
@@ -1,2 +1,2 @@
 -- later on 
-CREATE table IF NOT EXISTS id_not_null(id INT NOT NULL DEFAULT 1, name VARCHAR(256));
+CREATE TABLE IF NOT EXISTS id_not_null(id INT NOT NULL DEFAULT 1, name VARCHAR(256));
