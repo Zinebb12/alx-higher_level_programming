617aaa8e972678698a5ce5fad3f8e7ccbba4264d
Zinebb12 <zinebzaid@gmail.com>
2024-04-24 07:25:20 -0700

    Readme

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,1 +1,12 @@
+#!/usr/bin/node
 
+const fs = require('fs');
+const arg1 = process.argv[2];
+
+fs.readFile(`${arg1}`, 'utf8', function (err, data) {
+  console.log(data);
+
+  if (err) {
+    console.error(err);
+  }
+});
